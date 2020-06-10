b13a13905b83c82623afcdbc9f7330032198eb5a
Marwin <tecbravin@gmail.com>
2020-06-10 20:08:59 -0300

    teclado no browser mobile funcionando

---
+++ Assets/ContainerScript.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class InputfieldSlideScreen : MonoBehaviour {
+ 
+    // Assign canvas here in editor
+    public Canvas canvas;
+ 
+ 
+    // Used internally - set by InputfieldFocused.cs
+    public bool InputFieldActive = false;
+    public RectTransform childRectTransform;
+ 
+   void LateUpdate () {
+        if ((InputFieldActive)&&((TouchScreenKeyboard.visible)))
+        {
+            transform.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+ 
+            Vector3[] corners = {Vector3.zero,Vector3.zero,Vector3.zero,Vector3.zero};
+            Rect rect = RectTransformExtension.GetScreenRect(childRectTransform, canvas);
+            float keyboardHeight = TouchScreenKeyboard.area.height;
+ 
+            float heightPercentOfKeyboard = keyboardHeight/Screen.height*100f;
+            float heightPercentOfInput = (Screen.height-(rect.y+rect.height))/Screen.height*100f;
+ 
+ 
+            if (heightPercentOfKeyboard>heightPercentOfInput)
+            {
+                // keyboard covers input field so move screen up to show keyboard
+                float differenceHeightPercent = heightPercentOfKeyboard - heightPercentOfInput;
+                float newYPos = transform.GetComponent<RectTransform>().rect.height /100f*differenceHeightPercent;
+ 
+                Vector2 newAnchorPosition = Vector2.zero;
+                newAnchorPosition.y = newYPos;
+                transform.GetComponent<RectTransform>().anchoredPosition = newAnchorPosition;
+            } else {
+                // Keyboard top is below the position of the input field, so leave screen anchored at zero
+                transform.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+            }
+        } else {
+            // No focus or touchscreen invisible, set screen anchor to zero
+            transform.GetComponent<RectTransform>().anchoredPosition = Vector2.zero;
+        }
+        InputFieldActive = false;
+    }
+ 
+}
+   
+public static class RectTransformExtension {
+ 
+    public static Rect GetScreenRect(this RectTransform rectTransform, Canvas canvas) {
+ 
+        Vector3[] corners = new Vector3[4];
+        Vector3[] screenCorners = new Vector3[2];
+ 
+        rectTransform.GetWorldCorners(corners);
+ 
+        if (canvas.renderMode == RenderMode.ScreenSpaceCamera || canvas.renderMode == RenderMode.WorldSpace)
+        {
+            screenCorners[0] = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, corners[1]);
+            screenCorners[1] = RectTransformUtility.WorldToScreenPoint(canvas.worldCamera, corners[3]);
+        }
+        else
+        {
+            screenCorners[0] = RectTransformUtility.WorldToScreenPoint(null, corners[1]);
+            screenCorners[1] = RectTransformUtility.WorldToScreenPoint(null, corners[3]);
+        }
+ 
+        screenCorners[0].y = Screen.height - screenCorners[0].y;
+        screenCorners[1].y = Screen.height - screenCorners[1].y;
+ 
+        return new Rect(screenCorners[0], screenCorners[1] - screenCorners[0]);
+    }
+ 
+}
---
+++ Assets/GameControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameControl : MonoBehaviour
+{
+// In the Start function of this script I call a function on the webpage to let it know that the game has loaded.
+    void Start () {
+        #if (UNITY_WEBPLAYER || UNITY_WEBGL) && !UNITY_EDITOR
+        try {
+            Application.ExternalCall("GameControlReady");
+        } catch (System.Exception e) {
+            Debug.LogError("GameControlReady function not on webpage"+e);
+        }
+        #endif
+    }
+
+    // This function will be called from the webpage
+    public void FocusCanvas (string p_focus) {
+        #if !UNITY_EDITOR && UNITY_WEBGL
+        if (p_focus == "0") {
+            WebGLInput.captureAllKeyboardInput = false;
+        } else {
+            WebGLInput.captureAllKeyboardInput = true;
+        }
+        #endif
+    }
+}
